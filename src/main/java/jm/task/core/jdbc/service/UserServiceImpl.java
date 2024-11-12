d55bb34673c02520a8f2e0f10a70dcdd25f3be47
Nikita <worldofstyks@gmail.com>
2024-11-12 16:44:20 +0300

    redone overrides

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,26 +7,33 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
     private final UserDaoJDBCImpl daoJDBC = new UserDaoJDBCImpl();
+
+    @Override
     public void createUsersTable() {
         daoJDBC.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
         daoJDBC.dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         daoJDBC.saveUser(name, lastName, age);
     }
 
+    @Override
     public void removeUserById(long id) {
         daoJDBC.removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
         return daoJDBC.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
         daoJDBC.cleanUsersTable();
     }
