ecdaa25c61dde7db870f90ae57be788210d47396
Nikita <worldofstyks@gmail.com>
2024-11-14 12:46:44 +0300

    done hibernate

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,40 +1,42 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDaoJDBCImpl daoJDBC = new UserDaoJDBCImpl();
+    private final UserDao dao = new UserDaoHibernateImpl();
 
     @Override
     public void createUsersTable() {
-        daoJDBC.createUsersTable();
+        dao.createUsersTable();
     }
 
     @Override
     public void dropUsersTable() {
-        daoJDBC.dropUsersTable();
+        dao.dropUsersTable();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        daoJDBC.saveUser(name, lastName, age);
+        dao.saveUser(name, lastName, age);
     }
 
     @Override
     public void removeUserById(long id) {
-        daoJDBC.removeUserById(id);
+        dao.removeUserById(id);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return daoJDBC.getAllUsers();
+        return dao.getAllUsers();
     }
 
     @Override
     public void cleanUsersTable() {
-        daoJDBC.cleanUsersTable();
+        dao.cleanUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import java.util.Properties;
+
 public class Util {
     private static Connection connection;
+
+    private static SessionFactory sessionFactory;
+
     public static Connection getConnection() {
         try {
             String url = "jdbc:mysql://localhost:3306/schema_users";
@@ -17,5 +30,43 @@ public class Util {
         }
         return connection;
     }
+
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = getConfiguration();
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+    private static Configuration getConfiguration() {
+        Configuration configuration = new Configuration();
+
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(Environment.URL, "jdbc:mysql://localhost:3306/schema_users");
+        settings.put(Environment.USER, "root");
+        settings.put(Environment.PASS, "12345");
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+
+        settings.put(Environment.SHOW_SQL, "true");
+
+        settings.put(Environment.HBM2DDL_AUTO, "update");
+
+        configuration.setProperties(settings);
+        return configuration;
+    }
+
 
 }
